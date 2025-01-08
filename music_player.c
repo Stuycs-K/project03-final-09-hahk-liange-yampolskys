ec12ea15984670dab333b80503827f5083588fd5
el-cs <eliang50@stuy.edu>
2025-01-08 14:50:48 -0500

    Add basic music playing capabilities

--- music_player.c
+++ music_player.c
@@ -1,1 +1,23 @@
 #include "music_player.h"
+
+void check_mpg123() {
+
+}
+
+void play_file(char * file_name) {
+  pid_t p = fork();
+  if (p < 0) {
+    perror("fork fail");
+    exit(1);
+  } else if (p == 0) {
+    char * args[] = {"mpg123", "-q", file_name, NULL};
+    execvp(args[0], args);
+  } else {
+    printf("currently playing...\n");
+  }
+}
+
+int main() {
+  play_file("./beep-test.mp3");
+  return 0;
+}
--- music_player.h
+++ music_player.h
@@ -1,6 +1,14 @@
 #ifndef MUSIC_PLAYER_H
 #define MUSIC_PLAYER_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <fcntl.h>
 
+void check_mpg123();
+void play_file(char * file_name);
 
 #endif
