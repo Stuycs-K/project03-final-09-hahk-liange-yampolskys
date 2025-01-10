5ff41bb6d217156d73a81574f3a7aff236c922a1
Stella Yampolsky <stellagraceyampolksky@gmail.com>
2025-01-09 23:44:24 -0500

    finish functions, need to test but it's late

--- player.c
+++ player.c
@@ -1,31 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
 #include <string.h>
 #include "node.h"
 #include "library.h"
 #include "music_player.h"
-//reused music library code, modified nodes to hold files, ask for artist + title so most functions still work
-//maybe a default?
 
-//write basic placeholders for now, modify node + library code to actually work
-struct song_node skip(struct song_node song ){
-	return song -> next;
-}
-
-struct song_node shuffle_next(struct song_node ** library){
-	struct song_node start = chooseRandom(library[0]);
-	return start;
-}
-
-void shuffle(struct song_node ** library){
-	while(1){
-		file = struct song_node
-		play_file()
-	}
-}
+static int pause_flag = 0;
 
 static void sighandler(int signo) {
-    if ( signo ==  ){
-    
-          }
-  }
+    if (signo == SIGUSR1) {
+        pause_flag = !pause_flag;
+    } else if (signo == SIGINT) {
+        printf("\nTerminating playback...\n");
+        exit(0);
+    }
+}
+
+void play_file(char *filename) {
+    char *args[] = {"mpg123", filename, NULL};
+    execvp("mpg123", args);
+    perror("Error: execvp failed");
+    exit(1);
+}
+
+struct song_node *skip(struct song_node *current) {
+    if (current) {
+        return current->next;
+    }
+    return NULL;
+}
+
+struct song_node *shuffle_next(struct song_node **library) {
+    int bucket = rand() % 27;
+    return chooseRandom(library[bucket]);
+}
+
+void shuffle_play(struct song_node **library) {
+    struct song_node *current;
+    pid_t player_pid;
+
+    signal(SIGUSR1, sighandler);
+    signal(SIGINT, sighandler);
+
+    while (1) {
+        current = shuffle_next(library);
+        if (current) {
+            printf("Now playing: %s - %s\n", current->artist, current->title);
+            player_pid = fork();
+            if (player_pid == 0) {
+                play_file(current->filename);
+                exit(0);
+            } else if (player_pid > 0) {
+                while (1) {
+                    if (pause_flag) {
+                        kill(player_pid, SIGSTOP);
+                        while (pause_flag) {
+                            pause();
+                        }
+                        kill(player_pid, SIGCONT);
+                    }
+                    int status;
+                    if (waitpid(player_pid, &status, WNOHANG) > 0) {
+                        break;
+                    }
+                }
+            } else {
+                perror("Fork failed");
+                return;
+            }
+        } else {
+            printf("No songs available.\n");
+            break;
+        }
+    }
+}
