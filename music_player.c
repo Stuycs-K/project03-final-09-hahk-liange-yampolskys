2d82c173159193cfda833cad2a1eef0d358c85f8
el-cs <eliang50@stuy.edu>
2025-01-13 14:36:08 -0500

    Split into multiple functions

--- music_player.c
+++ music_player.c
@@ -4,37 +4,60 @@ void check_mpg123() {
 
 }
 
-void play_file(char * file_name) {
+void connect_player() {
+  to_player = open("to_player", O_WRONLY, 0);
+  from_player = open("from_player", O_RDONLY, 0);
+}
+
+int player_setup() {
   pid_t p = fork();
+  mkfifo("to_player", 0777);
+  mkfifo("from_player", 0777);
   if (p < 0) {
     perror("fork fail");
     exit(1);
   } else if (p == 0) {
     system("mpg123 -R < to_player > from_player"); // will probably switch to execvp and redirection
-    printf("finished playing.");
+    return 0;
   } else {
-    printf("currently playing...\n");
-    int to_player = open("to_player", O_WRONLY, 0);
-    int from_player = open("from_player", O_RDONLY, 0);
-    char buff[1000];
-    sprintf(buff, "L %s\n", file_name);
-    write(to_player, buff, 1000);
-    while (1) {
-        read(from_player, buff, 100);
-        //printf("%s\n", buff);
-        if (buff[1] == 'P' && buff[3] == '0') break;
-    }
-    close(to_player);
-    close(from_player);
-    printf("test\n");
+    connect_player();
+    return 1;
   }
 }
 
-int main() {
-  mkfifo("to_player", 0777);
-  mkfifo("from_player", 0777);
-  play_file("./beep-test.mp3");
+void disconnect_player() {
+  close(to_player);
+  close(from_player);
   remove("to_player");
   remove("from_player");
+}
+
+void read_player(char * b) {
+    read(from_player, b, sizeof(b));
+}
+
+void write_player(char * b) {
+  write(to_player, b, 1000);
+}
+
+void play_file(char * file_name) {
+  sprintf(buff, "L %s\n", file_name);
+  write_player(buff);
+}
+
+int check_finished_playing(char * b) {
+  return b[1] == 'P' && b[3] == '0';
+}
+
+int main() {
+  int is_main = player_setup();
+  if (is_main) {
+    play_file("./beep-test.mp3");
+    while (1) {
+        read_player(buff);
+        if (check_finished_playing(buff)) break;
+    }
+    disconnect_player();
+  }
   return 0;
 }
--- music_player.h
+++ music_player.h
@@ -10,7 +10,15 @@
 #include <time.h>
 #include <fcntl.h>
 
+int to_player, from_player;
+char buff[1000];
 void check_mpg123();
+void connect_player();
+int player_setup();
+void disconnect_player();
+void read_player(char * b);
+void write_player(char * b);
 void play_file(char * file_name);
+int check_finished_playing(char * b);
 
 #endif
