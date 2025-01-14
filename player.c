93e88a9c96a6b1c7f67bd296be1d18c94acc37ab
Stella <syampolsky50@stuy.edu>
2025-01-14 13:29:38 -0600

    rewrite methods to be compatible

--- player.c
+++ player.c
@@ -8,27 +8,23 @@
 #include "library.h"
 #include "music_player.h"
 
-static int pause_flag = 0;
 
 static void sighandler(int signo) {
-    if (signo == SIGUSR1) {
-        pause_flag = !pause_flag;
-    } else if (signo == SIGINT) {
+	 if (signo == SIGINT) {
         printf("\nTerminating playback...\n");
         exit(0);
     }
 }
 
-struct song_node *skip(struct song_node *current) {
+char* skip(struct song_node *current) {
     if (current) {
-        return current->next;
+        return current->next->filename;
     }
     return NULL;
 }
-
-struct song_node *shuffle_next(struct song_node **library) {
+char *shuffle_next(struct song_node **library) {
     int bucket = rand() % 27;
-    return chooseRandom(library[bucket]);
+    return chooseRandom(library[bucket])->filename;
 }
 
 void shuffle_play(struct song_node **library) {\
@@ -36,7 +32,6 @@ void shuffle_play(struct song_node **library) {\
     struct song_node *current;
     pid_t player_pid;
 
-    signal(SIGUSR1, sighandler);
     signal(SIGINT, sighandler);
 
     while (1) {
@@ -45,23 +40,10 @@ void shuffle_play(struct song_node **library) {\
             printf("Now playing: %s - %s\n", current->artist, current->title);
             player_pid = fork();
             if (player_pid == 0) {
-                play_file(current->filename);
+                play_file(current);
                 exit(0);
-            } else if (player_pid > 0) {
-                while (1) {
-                    if (pause_flag) {
-                        kill(player_pid, SIGSTOP);
-                        while (pause_flag) {
-                            pause();
-                        }
-                        kill(player_pid, SIGCONT);
-                    }
-                    int status;
-                    if (waitpid(player_pid, &status, WNOHANG) > 0) {
-                        break;
-                    }
-                }
-            } else {
+            }
+             else {
                 perror("Fork failed");
                 return;
             }
