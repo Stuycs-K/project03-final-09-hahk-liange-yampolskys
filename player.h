041b89e908f03bde7fcc0b8b799f6908b042edb8
Stella <syampolsky50@stuy.edu>
2025-01-15 13:22:47 -0600

    fix header file

--- player.h
+++ player.h
@@ -6,5 +6,7 @@ static void sighandler(int signo);
 struct song_node *skip(struct song_node *current);
 struct song_node *shuffle_next(struct song_node **library);
 void shuffle_play(struct song_node ** library);
-
+void loop(struct song_node **library, struct song_node *song);
+void queueSongs(struct song_node **library, struct song_node *queueHead);
+void addToQueue(struct song_node **queueHead, struct song_node *newSong);
 #endif
