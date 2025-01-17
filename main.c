3f24bef825e34d7ae195eed1194189e0d3d21806
Keith H <khah50@stuy.edu>
2025-01-17 14:45:33 -0500

    added more user functions like loop and skip and wokred on delete and remove song

--- main.c
+++ main.c
@@ -5,24 +5,27 @@
 #include "library.h"
 #include "file.h"
 
-#define list_name
+#define list_name "playlist.txt"
 
 int main(){
   struct song_node **library = init();
   int option = 0;
   char input[100];
-  while(option != 9){
+  while(option != 12){
     printf("\n__________________________\n");
     printf("\nMenu:\n");
-    printf("1. View playlist\n");
+    printf("1. Load & view playlist\n");
     printf("2. Play\n");
     printf("3. Play a song\n");
-    printf("4. Add a song\n");
-    printf("5. Look up a song\n");
-    printf("6. Look up an artist\n");
-    printf("7. Create new playlist\n");
-    printf("8. Delete playlist\n");
-    printf("9. Save and exit\n");
+    printf("4. Skip song\n"); //here too
+    printf("5. Add song\n");
+    printf("6. Remove song\n");
+    printf("7. Look up a song\n");
+    printf("8. Look up an artist\n");
+    printf("9. Loop playlist\n"); //i think this is another person's funciton
+    printf("10. Create new playlist\n");
+    printf("11. Delete playlist\n");
+    printf("12. Save and exit\n");
     printf("Choose an option: ");
 
     if(fgets(input, sizeof(input), stdin)){
@@ -32,7 +35,7 @@ int main(){
         }
     }
 
-    if(option == 1){ //view
+    if(option == 1){ //load & view
       printf("\nViewing playlist:\n");
       print_library(library);
     }
@@ -45,7 +48,12 @@ int main(){
 
       }
 
-    else if(option == 4){ //add song
+    else if(option == 4){ //skip song
+
+    }
+
+
+    else if(option == 5){ //add song
       char artist[100];
       char title[100];
       printf("Enter artist: ");
@@ -66,7 +74,28 @@ int main(){
       printf("\nSong added.\n");
     }
 
-    else if(option == 5){ //look up song
+    else if(option == 6){ //remove song
+      char artist[100];
+      char title[100];
+      printf("Enter artist: ");
+      if(fgets(artist, sizeof(artist), stdin)){
+          int length = strlen(artist);
+          if(length > 0 && artist[length - 1] == '\n'){
+              artist[length-1] = '\0';
+          }
+      }
+      printf("Enter title: ");
+      if(fgets(title, sizeof(title), stdin)){
+          int length = strlen(title);
+          if(length > 0 && title[length-1] == '\n'){
+              title[length-1] = '\0';
+          }
+      }
+      delete_song(library, artist, title);
+      printf("\nSong removed.\n");
+    }
+
+    else if(option == 7){ //look up song
       char artist[100];
       char title[100];
       printf("Enter artist: ");
@@ -92,32 +121,45 @@ int main(){
       }
     }
 
-    else if(option == 6){ //look up artist
+    else if(option == 8){ //look up artist
       char artist[100];
       printf("Enter artist: ");
       if(fgets(artist, sizeof(artist), stdin)){
         int length = strlen(artist);
-          if(length > 0 && artist[length - 1] == '\n'){
-            artist[length -1] = '\0';
-          }
+        if(length > 0 && artist[length - 1] == '\n'){
+          artist[length -1] = '\0';
+        }
       }
       printf("\nSongs by %s:\n", artist);
       print_artist(library, artist);
     }
 
-    else if(option == 7){ //create new playlist
+    else if(option == 9){ //loop playlist
+
+    }
+
+    else if(option == 10){ //create new playlist
     //  char list_name[100];
       printf("Enter playlist name: ");
       fgets(list_name, sizeof(list_name), stdin);
       save_library(library, list_name);
-      printf("%s saved to playlists.\n", list_name);
+      printf("\n%s saved to playlists.\n", list_name);
       }
 
-    else if(option == 8){ //delete playlist of choice
+    else if(option == 11){ //delete playlist of choice
+      char name[100];
+      printf("Enter playlist name: ");
+      if(fgets(name, sizeof(name), stdin)){
+        int length = strlen(name);
+        if(length > 0 && name[length - 1] == '\n'){
 
+        }
       }
 
-    else if(option == 9){ //save and exit
+      printf("\n%s deleted.\n", name)
+  }
+
+    else if(option == 12){ //save and exit
       save_library(library, list_name);
       printf("\nPlaylist saved.\n");
       reset(library);
@@ -129,34 +171,5 @@ int main(){
     }
   }
 
-
-
-
-
-    /*
-    add(library, "The Beatles", "Hey Jude");
-    add(library, "RHCP", "Can't Stop");
-    add(library, "The Beatles", "Let It Be");
-    add(library, "Pink Floyd", "Comfortably Numb");
-    add(library, "The Rolling Stones", "Paint It Black");
-    add(library, "Pink Floyd", "Wish You Were Here");
-
-    printf("Music Library:\n");
-    print_library(library);
-
-    char *filename = "library.txt";
-    printf("\nSaving library to '%s'...\n", filename);
-    save_library(library, filename);
-
-    reset(library);
-
-    printf("\nLoading library from '%s'...\n", filename);
-    load_library(library, filename);
-
-    printf("\nLoaded Music Library:\n");
-    print_library(library);
-
-    reset(library);
-*/
     return 0;
 }
