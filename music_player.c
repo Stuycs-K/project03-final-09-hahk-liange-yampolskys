9e58f844c9f6c1fac1b38424cfe1a37c855e0b8a
el-cs <eliang50@stuy.edu>
2025-01-18 17:07:09 -0500

    Add back check_frame_info

--- music_player.c
+++ music_player.c
@@ -86,6 +86,13 @@ int get_to_player(){
   return to_player;
 }
 
+struct frame_info * check_frame_info(char * b) {
+  if (b[1] != 'F') return NULL;
+  struct frame_info * ret = malloc(sizeof(struct frame_info));
+  sscanf(b + 3, "%d %d %f %f", (int *)ret, (int *)ret + 1, (float *)ret + 2, (float *)ret + 3);
+  return ret;
+}
+
 int interactive_player(char * file_name, char * artist, char * title, float volume) {
   printf("[ ] pause/resume, [a/d] jump left/right, [0-9] jump to position, [w/s] increase/decrease volume, [e] skip, [q] quit\nNow playing: %s - %s\n\n", artist, title);
   play_file(file_name);
