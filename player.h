8e3289de87830dcc818fb5e910925f9a8db315a8
Stella Yampolsky <stellagraceyampolksky@gmail.com>
2025-01-18 17:39:27 -0500

    fisher yates, fixes

--- player.h
+++ player.h
@@ -1,12 +1,12 @@
 #ifndef PLAYER_H
-
 #define PLAYER_H
 
 void sighandler(int signo);
-struct song_node *skip(struct song_node *current);
-struct song_node *shuffle_next(struct song_node **library);
+char* skip(struct song_node *current);
+void fisherYatesShuffle(struct song_node **array, int size);
 void shufflePlay(struct song_node ** library);
 void loop(struct song_node **library, struct song_node *song);
 void queueSongs(struct song_node **library, struct song_node *queueHead);
 void addToQueue(struct song_node **queueHead, struct song_node *newSong);
+
 #endif
